8d8ed0ef0d41de118b97b2b4220939b19a62d49c
Darel <darel.gutierrez12@gmail.com>
2023-03-30 11:22:20 +0800

    Database auto create

--- class/config.php
+++ class/config.php
@@ -4,15 +4,26 @@
         private $user = 'root';
         private $pass = 'Allen is Great 200%';
         public $pdo = null;
+        private $dbname = 'todolist';
 
         public function con(){
             try {
-                $this->pdo = new PDO("mysql:host=localhost;dbname=todolist", $this->user, $this->pass);
-
+                $this->pdo = new PDO("mysql:host=localhost;", $this->user, $this->pass);
                 $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                // echo "Connected successfully";
+
+                $this->pdo->exec("CREATE DATABASE IF NOT EXISTS $this->dbname");
+                $this->pdo->exec("USE $this->dbname");
+                $this->pdo->exec("CREATE TABLE IF NOT EXISTS users(
+                  id int(6) AUTO_INCREMENT PRIMARY KEY,
+                  item VARCHAR(50) NOT NULL,
+                  stats VARCHAR(50),
+                  date_added DATETIME DEFAULT CURRENT_TIMESTAMP,
+                  date_completed datetime
+                )");
+
+
+
               } catch(PDOException $e) {
-                    // die($e->getMessage());
                 echo "Connection failed: " . $e->getMessage();
               }
                return $this->pdo;
